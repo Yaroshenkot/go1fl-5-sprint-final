1b8c5d56cf0bd15da9ade6363507b26642d44198
Татьяна <yaroshenkot@yandex.ru>
2025-12-10 12:37:57 +0300

    изменение проекта спринта5 по условиям задания

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,66 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
 	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return errors.New("неверный формат строки, ожидается: `шаги, длительность`")
+	}
+	stepsStr := strings.TrimSpace(parts[0])
+	steps, err := strconv.Atoi(stepsStr)
+	if err != nil {
+		return fmt.Errorf("ошибка преобразования количества шагов: %v", err)
+	}
+
+	if steps <= 0 {
+		return errors.New("количество шагов должно быть положительным")
+	}
+
+	ds.Steps = steps
+
+	durationStr := strings.TrimSpace(parts[1])
+	duration, err := time.ParseDuration(durationStr)
+
+	if err != nil {
+		return fmt.Errorf("ошибка преобразования длительности: %v", err)
+	}
+
+	if duration <= 0 {
+		return errors.New("длительность должна быть положительной")
+	}
+
+	ds.Duration = duration
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", fmt.Errorf("ошибка расчета калорий: %v", err)
+	}
+
+	info := fmt.Sprintf("количество шагов: %d\n"+"Дистанция составила %.2f км.\n"+"Вы сожгли %.2f кал.", ds.Steps, distance, calories)
+	return info, nil
 }
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
 
@@ -14,16 +15,72 @@ const (
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, errors.New("количество шагов должно быть положительным")
+	}
+	if weight <= 0 {
+		return 0, errors.New("вес должен быть положительным")
+	}
+	if height <= 0 {
+		return 0, errors.New("рост должен быть положительным")
+	}
+	if duration <= 0 {
+		return 0, errors.New("продолжительность должна быть положительной")
+	}
+	speed := MeanSpeed(steps, height, duration)
+	if speed == 0 {
+		return 0, errors.New("невозможно расчитать скорость с указанными параметрами")
+	}
+	durationMinutes := duration.Minutes()
+	calories := (weight * speed * durationMinutes) / minInH
+	calories *= walkingCaloriesCoefficient
+
+	return calories, nil
+
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, errors.New("количество шагов должно быть положительным")
+	}
+	if weight <= 0 {
+		return 0, errors.New("вес должен быть положительным")
+	}
+	if height <= 0 {
+		return 0, errors.New("рост должен быть положительным")
+	}
+	if duration <= 0 {
+		return 0, errors.New("продолжительность должна быть положительной")
+	}
+	speed := MeanSpeed(steps, height, duration)
+	if speed == 0 {
+		return 0, errors.New("невозможно расчитать скорость с указанными параметрами")
+	}
+	durationMinutes := duration.Minutes()
+	calories := (weight * speed * durationMinutes) / minInH
+	return calories, nil
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if steps <= 0 || height <= 0 || duration <= 0 {
+		return 0
+	}
+	distance := Distance(steps, height)
+	durationHours := duration.Hours()
+	if durationHours == 0 {
+		return 0
+	}
+	return distance / durationHours
 }
 
 func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
+	if steps <= 0 || height <= 0 {
+		return 0
+	}
+	stepLength := height * stepLengthCoefficient
+	distanceMeters := float64(steps) * stepLength
+	return distanceMeters / mInKm
 }
